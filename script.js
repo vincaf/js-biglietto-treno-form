1b01619ad469a681709ebfad55d453f36362e87a
vincaf <vincenzocafassog@gmail.com>
2022-06-15 16:35:19 +0200

    converting data into integers

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Inizializzazione variabile bottone di invio dati
 const buttonSubmit = document.querySelector("#input-submit");
 
 buttonSubmit.addEventListener('click', function(){
     console.log(document.getElementById('input-userKm').value);
     console.log(document.getElementById('input-userAge').value);
 });
+
+// Conversione dei dati in numeri interi
+const userKm = parseInt(document.querySelector("#input-userKm"));
+const userAge = parseInt(document.querySelector("#input-userAge"));
+console.log(userKm);
+console.log(userAge);
