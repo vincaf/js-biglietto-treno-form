e0857ac31241009d7f8f42704714cbd386dface2
vincaf <vincenzocafassog@gmail.com>
2022-06-15 16:31:42 +0200

    event click on button JS

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonSubmit = document.querySelector("#input-submit");
+
+buttonSubmit.addEventListener('click', function(){
+    console.log(document.getElementById('input-userKm').value);
+    console.log(document.getElementById('input-userAge').value);
+});
